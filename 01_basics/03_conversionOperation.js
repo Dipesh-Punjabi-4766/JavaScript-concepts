3522ade0a5033f48d5de68e785aa600a32a765cb
Dipesh-Punjabi-4766 <punjabi4766@gmail.com>
2024-07-07 15:12:09 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,30 @@ console.log(booleanIsloggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// *****Operations*****//
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello  "
+let str2="Dipesh"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+5+"2");
