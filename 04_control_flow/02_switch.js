19e81d93861fd6126e6e765fbf98037eb85724a0
Dipesh Punjabi <punjabi4766@gmail.com>
2024-07-29 13:37:39 +0000

    Switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,53 @@
+//basic syntax of switch controll flow
+/* switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+// for basic understanding assume a key which we want to try to match with multiple lock.... whenever the key gets the perfect lock it implements the code 
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+const month_no = 2
+
+switch (month_no) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
