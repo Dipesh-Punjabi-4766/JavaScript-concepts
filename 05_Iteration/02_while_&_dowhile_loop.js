762bad6ed9e424252ff5945951040773ab250f8b
Dipesh Punjabi <punjabi4766@gmail.com>
2024-07-31 09:30:46 +0000

    While loop and Do while loop

--- 05_Iteration/02_while_&_dowhile_loop.js
+++ 05_Iteration/02_while_&_dowhile_loop.js
@@ -1,18 +1,23 @@
+//While Loop
+//while loop check the condition first and then implements the given code
 let index = 0
-// while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
-// }
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
 
 let myArray = ['flash', "batman", "superman"]
 
 let arr = 0
 while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
+    console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1
 }
 
-let score = 11
+//Do-While
+//Do-While loop implements the given code once and then check the condition for re implementation of the code based o condition 
+
+let score = 10
 
 do {
     console.log(`Score is ${score}`);
