98afb297b01ea2b3689e9d839b60980be4d70ef1
Dipesh Punjabi <punjabi4766@gmail.com>
2024-08-03 04:50:54 +0000

    For in Loop

---
+++ 05_Iteration/04_For_in_loop.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(`${key} is an index and key no for this ${programming[key]} data`);
+    //Key of array are the index no of the array hanece the above line will print indexes of the data
+}
+
+//For in loop on Map
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// Map are non iterable to for is loop hancewe get an empty output
